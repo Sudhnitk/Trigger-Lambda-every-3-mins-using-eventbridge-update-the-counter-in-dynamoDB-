741aa339888d03de5e95ed5498bcf07db08696fc
sudhnitk <sudhnit.kaur@ellucian.com>
2023-12-08 16:21:31 +0530

    Updated

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 
 
-const { DynamoDBClient, UpdateItemCommand } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBClient, UpdateItemCommand, GetItemCommand } = require('@aws-sdk/client-dynamodb');
 const { EventBridgeClient, PutEventsCommand } = require('@aws-sdk/client-eventbridge');
 
 const ddbClient = new DynamoDBClient({ region: 'us-east-1'});
@@ -8,10 +8,31 @@ const eventBridgeClient = new EventBridgeClient({ region: 'us-east-1'});
 
 module.exports.eventCounter = async (event) => {
     const currentTime = new Date().toISOString();
-    const params = {
+    const checkParams = {
         TableName: 'eventCounter',
         Key: {
-            'counterId': { S: 'play' }
+            'counterId': { S: 'super' }
+        },
+        ProjectionExpression: 'counterEnabled' 
+    };
+
+    try {
+        const getItemCommand = new GetItemCommand(checkParams);
+        const getItemResponse = await ddbClient.send(getItemCommand);
+
+        if (
+            getItemResponse.Item &&
+            getItemResponse.Item.counterEnabled &&
+            getItemResponse.Item.counterEnabled.BOOL === false // Check if counterEnabled is false
+        ) {
+            console.log('Counter is manually disabled. Skipping increment.');
+            return { statusCode: 200, body: 'Counter stopped manually' };
+        }
+        const params = {
+        TableName: 'eventCounter',
+        Key: {
+            'counterId': { S: 'super' }
+
         },
         UpdateExpression: 'SET counterValue = if_not_exists(counterValue, :initial) + :incr, ' +
                           'updatedAt = :updatedAt',
@@ -23,7 +44,7 @@ module.exports.eventCounter = async (event) => {
         ReturnValues: 'UPDATED_NEW'
     };
 
-    try {
+    
         const updateCommand = new UpdateItemCommand(params);
         const updatedCounter = await ddbClient.send(updateCommand);
 
